7b2616e5dc2aaa4c7fc3430684a33528cac7708d
AndreaBonalumi <andrea.bonalumi@si2001.it>
2023-07-24 12:34:30 +0200

    rebasing

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -20,30 +20,6 @@ export class LoginComponent implements OnInit {
   }
   ngOnInit() {
 
-    console.log("prova di branching e rebasing (commit su master)")
-    console.log("prova di branching e committing più cose (secondo commit di master)")
-    console.log("prova di branching e committing più cose")
-    console.log("prova di branching e rebasing (commit su master)")
-    console.log("prova di branching e committing più cose (secondo commit di master)")
-    console.log("prova di branching e solo commit, senza push")
-    console.log("prova di branching e rebasing")
-    console.log("prova di branching e committing più cose")
-    console.log("prova di branching e solo commit, senza push")
-    console.log("prova di branching e rebasing")
-    console.log("prova di branching e committing più cose")
-    console.log("prova di branching e solo commit, senza push")
-    console.log("prova di branching e rebasing")
-    console.log("prova di branching e committing più cose")
-    console.log("prova di branching e solo commit, senza push")
-    console.log("prova di branching e rebasing")
-    console.log("prova di branching e committing più cose")
-    console.log("prova di branching e solo commit, senza push")
-
-
-    console.log("committing sul branch")
-    console.log("altro commit sul branch")
-
-
     if ((localStorage.getItem("jwtToken") != null ||
       localStorage.getItem("jwtToken") != "" ||
       localStorage.getItem("jwtToken") != undefined))
