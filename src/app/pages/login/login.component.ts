c43f984904c88f637bd15942e5720d7ce35df29a
AndreaBonalumi <andrea.bonalumi@si2001.it>
2023-07-24 16:19:56 +0200

    branch2

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -21,6 +21,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
 
     console.log("branch1")
+    console.log("branch2")
 
     if ((localStorage.getItem("jwtToken") != null ||
       localStorage.getItem("jwtToken") != "" ||
