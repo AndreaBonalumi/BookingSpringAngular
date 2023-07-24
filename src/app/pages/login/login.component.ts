0fe953750870fc166da21173e2f4142e046f7e13
AndreaBonalumi <andrea.bonalumi@si2001.it>
2023-07-24 16:46:17 +0200

    messaggio2

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -21,6 +21,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
 
     console.log("messaggio")
+    console.log("messaggio2")
 
     if ((localStorage.getItem("jwtToken") != null ||
       localStorage.getItem("jwtToken") != "" ||
