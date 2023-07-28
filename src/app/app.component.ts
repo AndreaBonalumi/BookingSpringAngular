ee42484796a1a3c900a55c9a73be255368fbcebb
AndreaBonalumi <andrea.bonalumi@si2001.it>
2023-07-28 09:42:07 +0200

    navbar con tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {AfterContentChecked, Component, OnInit} from '@angular/core';
 import {User} from "./interfaces/user";
 import {UserService} from "./services/user.service";
+import {DomSanitizer} from "@angular/platform-browser";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +11,10 @@ export class AppComponent implements OnInit, AfterContentChecked{
   title = 'BookingSpringAngular';
   token: string | null = null
   user ?: User
+  profilePhoto: any
   logoutFlag: boolean = false
   noAuthFlag: boolean = false
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private sanitizer: DomSanitizer) {
   }
   ngAfterContentChecked(): void {
     if (localStorage.getItem("jwtToken") == null && this.token != null) {
@@ -44,6 +46,12 @@ export class AppComponent implements OnInit, AfterContentChecked{
         .subscribe({
           next: user => {
             this.user = user
+            if (this.user!.profilePhoto) {
+              this.userService.getProfileImage(user.idUser).subscribe(blob => {
+                let objectURL = URL.createObjectURL(blob);
+                this.profilePhoto = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+              });
+            }
           },
           error: () => {
             localStorage.clear()
